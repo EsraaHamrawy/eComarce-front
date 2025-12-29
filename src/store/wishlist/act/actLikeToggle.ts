a1ed010c9db6356c7578502a16faf60fdb4a2071
EsraaHamrawy <Esraa889675@gmail.com>
2025-12-29 12:33:23 +0200

    upload project

---
+++ src/store/wishlist/act/actLikeToggle.ts
@@ -0,0 +1,39 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { axiosErrorHandler } from "@utils";
+
+const actLikeToggle = createAsyncThunk(
+  "wishlist/actLikeToggle",
+  async (productId: number, thunkAPI) => {
+    const { rejectWithValue, getState} = thunkAPI;
+
+    try {
+      const state : unknown = getState() ;
+      const userId = state.auth.user?.id;
+
+      if(!userId){
+        return rejectWithValue("User not authenticated");
+      }
+
+      const isRecordExist = await axios.get(
+        `/wishlist?userId=${userId}&productId=${productId}`
+      );
+
+
+      if (isRecordExist.data.length > 0) {
+        await axios.delete(`/wishlist/${isRecordExist.data[0].id}`);
+        return { type: "remove", id: productId };
+      } else {
+        await axios.post("/wishlist", {
+           userId, 
+           productId 
+          });
+        return { type: "add", id: productId};
+      }
+    } catch (error) {
+      return rejectWithValue(axiosErrorHandler(error));
+    }
+  }
+);
+
+export default actLikeToggle;
